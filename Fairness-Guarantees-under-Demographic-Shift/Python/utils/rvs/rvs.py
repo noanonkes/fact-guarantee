7ace276e4a4525b1e9f22685ad31a305d60f9259
Zjwas <zjosvdsande@hotmail.com>
2023-01-16 16:38:37 +0100

    Fixed Nonetype bound error

--- Fairness-Guarantees-under-Demographic-Shift/Python/utils/rvs/rvs.py
+++ Fairness-Guarantees-under-Demographic-Shift/Python/utils/rvs/rvs.py
@@ -418,7 +418,8 @@ class ConstraintManager():
 				ln, un = ConstraintManager.bound_expression(_E, bounds)
 				l, u = max(l,ln), max(u,un)
 			return (l,u)
-		if isinstance(E, expressions.MaxExpression):
+		# Adjusted to NANMax
+		if isinstance(E, expressions.NANMaxExpression):
 			l, u = ConstraintManager.bound_expression(E._terms[0], bounds)
 			for _E in E._terms[1:]:
 				ln, un = ConstraintManager.bound_expression(_E, bounds)
